2795ab95d785b2946322b6bdcb8b8b9ed73ae217
Matteo Piccini <gnappo.1@live.it>
2025-01-16 11:10:50 -0800

    declare global variable

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ console.log("I am in the master branch and this is bad!")
 console.log("I am in the master branch and this is bad!")
 console.log("I am in the master branch and this is bad!")
 
+let name =  "Flatiron School"
